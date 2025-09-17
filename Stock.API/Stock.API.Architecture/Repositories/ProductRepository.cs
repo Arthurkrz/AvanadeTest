ca2c612127b0482b23cc41457e8a2a0f9e3cdade
Arthur K <arthurk031@outlook.com>
2025-09-16 21:22:04 -0300

    Refact/Erro em merge

---
+++ Stock.API/Stock.API.Architecture/Repositories/ProductRepository.cs
@@ -0,0 +1,63 @@
+﻿using Stock.API.Core.Common;
+using Stock.API.Core.Contracts.Repository;
+using Stock.API.Core.Entities;
+
+namespace Stock.API.Architecture.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Response<Product> Create(Product product)
+        {
+            try { _context.Products.Add(product); }
+            catch (Exception ex) { return Response<Product>.Ko(0, new List<string> { ex.Message }); }
+
+            _context.SaveChanges();
+            return Response<Product>.Ok(product);
+        }
+
+        public IEnumerable<Product> GetAll() =>
+            _context.Set<Product>().AsQueryable();
+
+        public Response<Product> GetById(Guid productId)
+        {
+            try 
+            { 
+                var entity = _context.Products.Find(productId); 
+
+                if (entity == null)
+                    return Response<Product>.Ko(0, new List<string> { "Product not found." });
+
+                return Response<Product>.Ok(entity);
+            }
+
+            catch (Exception ex) { return Response<Product>.Ko(0, new List<string> { ex.Message }); }
+        }
+
+        public Response<Product> UpdateStock(Guid productId, int newAmountInStock)
+        {
+            Product? existingEntity = new();
+
+            try
+            {
+                existingEntity = _context.Products.Find(productId);
+                if (existingEntity == null)
+                    return Response<Product>.Ko(0, new List<string> { "Product not found." });
+
+                _context.Entry(existingEntity).CurrentValues
+                              .SetValues(new { AmountInStock = newAmountInStock });
+            }
+
+            catch (Exception ex) { return Response<Product>.Ko(0, new List<string> { ex.Message }); }
+
+            _context.SaveChanges();
+            return Response<Product>.Ok(existingEntity);
+        }
+    }
+}
